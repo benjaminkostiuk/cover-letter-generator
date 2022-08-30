7fdcc175cedeefa56dc5f7f2d58e3ceae9f31c94
Ben Kostiuk <benkostiuk1@gmail.com>
2022-08-30 11:31:22 -0400

    Fix executable to use script path

--- ccl.sh
+++ ccl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Backup shell script for windows
-python 'src/create-cover-letter.py' $1
+# Get full path to this executable
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# Shell script for windows
+python "$SCRIPT_DIR/src/create-cover-letter.py" $1
